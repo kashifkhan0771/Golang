9d5c06f79c9ac9d9d803594f5270321c6394a6c7
kashif khan <kashif.khan@wanclouds.net>
2021-03-18 11:26:03 +0500

    while loop example added

---
+++ whileLoop/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main(){
+	// go do not have any while loop, but we can use for loop like while, wonder how see example below
+	i := 1
+	for i <= 10{
+		fmt.Println("I Value IS : ", i)
+		i ++
+	}
+}
